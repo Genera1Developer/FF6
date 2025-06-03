fa29b92e0273e9b2131d7389cf9e07e202797184
UVUE <wanted.glitch@gmail.com>
2025-06-03 09:52:51 -0400

    Update blocklist.js

--- build/blocklist.js
+++ build/blocklist.js
@@ -1,9 +1,28 @@
-// List of blocked words so no bad
-const BLOCKED_TERMS = [
-  "porn", "nazi", "drugs", "violence", "malware", "exploit"
-];
+const BLOCKED_TERMS = {
+  porn: [
+    "porn", "sex", "xxx", "nude", "hentai", "nsfw", "onlyfans", "erotic", "fetish", "bdsm", "milf", "teen", "pussy", "dick", "cock", "boobs", "anal", "cum", "blowjob", "orgy", "gangbang", "naked", "bondage", "fuck", "rape"
+  ],
+  gore: [
+    "gore", "blood", "dismember", "decapitate", "snuff", "mutilate", "disembowel", "exsanguinate", "carcass", "torture", "behead", "massacre", "cannibal", "crucify", "impale", "hang", "strangle", "dismemberment", "death", "murder", "kill"
+  ],
+  drugs: [
+    "cocaine", "meth", "heroin", "lsd", "ecstasy", "fentanyl", "weed", "marijuana", "cannabis", "opioid", "opioids", "amphetamine", "crack", "pcp", "shrooms", "acid", "hashish", "ketamine", "drug", "crack"
+  ],
+  illegal: [
+    "child", "cp", "torrent", "dark web", "silk road", "hack tool", "piracy", "counterfeit", "fraud", "phishing", "scam", "weapon", "gun", "firearm", "bomb", "explosive", "drug dealer", "gun", "tor", "illegal", "child", "conterfeit", "hack", "toolkit", "warez"
+  ],
+  violence: [
+    "kill", "murder", "rape", "assault", "torture", "shoot", "stab", "beat", "abuse", "slaughter", "lynch", "terrorist", "terrorism", "mass shooting", "genocide", "war", "battle", "crime", "fight", "violence", "blood", "homocide", "suicide", "die", "genocide", "massacre", "shoot", "shooting"
+  ],
+  malware: [
+    "malware", "exploit", "keylogger", "rat", "trojan", "virus", "worm", "spyware", "ransomware", "backdoor", "botnet", "ddos", "phishing", "rootkit"
+  ],
+  hate: [
+    "nazi", "racist", "kkk", "white power", "hate crime", "supremacist", "antisemitic", "homophobic", "xenophobic", "islamophobe", "bigot", "terrorist", "neo-nazi", "white nationalist"
+  ]
+};
 
 function isBlocked(query) {
   const lowered = query.toLowerCase();
-  return BLOCKED_TERMS.some(word => lowered.includes(word));
+  return Object.values(BLOCKED_TERMS).flat().some(term => lowered.includes(term));
 }
